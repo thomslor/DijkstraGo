7dba94810dbd724ba63f444d33c6b76dda9d2ea6
T-lorrain <T-lorrain@users.noreply.github.com>
2020-10-14 16:13:26 +0200

    Update lecture.go

--- lecture.go
+++ lecture.go
@@ -5,14 +5,26 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type nd struct {
+	nom string
+}
 
 type Lien struct {
 	dep   string
 	fin   string
 	poids int
+	id    int
 }
+
+func getGraph() {
+
+}
+
+var maMap map[string]Lien
 
 func main() {
 	f, err := os.Open("graph.txt")
@@ -34,11 +46,15 @@ func main() {
 		res1 := tsep[0]
 		res2 := tsep[1]
 		res3 := tsep[2]
-		//res4 := tsep[3]
-		//res4, err := strconv.ParseInt("55", 10, 64)
-		//if err == nil {
-		//	fmt.Println(res4)
-		//}
-		fmt.Println("Depart: ", res1, "|", "Fin: ", res2, "|", "ID: ", res3, "|", "Distance: ", res4)
+		resq := strings.TrimSuffix(tsep[3], "\r\n")
+		res4, err := strconv.Atoi(resq)
+		if err == nil {
+			//fmt.Println(res4)
+		}
+		fmt.Println("Depart:", res1, "|", "Fin:", res2, "|", "ID: ", res3, "|", "Distance:", res4)
 	}
+	if err != nil {
+		fmt.Printf("DEBUG ERROR TYPE %d", err)
+	}
+
 }
