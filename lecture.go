534c3d91bb7a5ab7688635716cf729c5fc8b12ab
joyeux-nael <72874450+joyeux-nael@users.noreply.github.com>
2020-11-06 08:28:10 +0100

    Update lecture.go

--- lecture.go
+++ lecture.go
@@ -10,26 +10,28 @@ import (
 	"strings"
 )
 
-//type nd struct {
-//	nom string
-//}
-
-type Lien struct {
-	dep   string
-	fin   string
-	poids int
-	id    int
+type Nd struct {
+	nom string
 }
 
-func getGraph(graph map[string][]Lien) {
+type Lien struct {
+	dep   *Nd
+	fin   *Nd
+	poids int
+}
+
+graph := makeGraph()
+
+const Infinity = int(^uint(0) >> 1)
+
+func getGraph(graph map[*Nd][]Lien) {
 	for i := range graph {
 		fmt.Println(graph[i])
 	}
 }
 
-func makeGraph() map[string][]Lien {
-	graph := make(map[string][]Lien)
-	idl := 0
+func makeGraph() map[*Nd][]Lien {
+	graph := make(map[*Nd][]Lien)
 	f, err := os.Open("graph.txt")
 	defer f.Close()
 	if err != nil {
@@ -49,16 +51,16 @@ func makeGraph() map[string][]Lien {
 			log.Fatal(err)
 		}
 		tsep := strings.Split(line, ";")
-		res1 := tsep[0]
-		res2 := tsep[1]
+		res1 := &Nd{nom: tsep[0]}
+		res2 := &Nd{nom: tsep[1]}
 		resq := strings.TrimSuffix(tsep[2], "\r\n")
 		res3, err := strconv.Atoi(resq)
 		if err == nil {
 			//fmt.Println(res3)
 		}
 
-		lien := Lien{res1, res2, res3, idl}
-		idl += 1
+		lien := Lien{res1, res2, res3}
+
 		graph[res1] = append(graph[res1], lien)
 
 		//fmt.Println("Depart:", res1, "|", "Fin:", res2, "|", "ID: ", res3, "|", "Distance:")
@@ -67,12 +69,27 @@ func makeGraph() map[string][]Lien {
 
 }
 
+func ListeNd(graph map[*Nd][]Lien) []Nd {
+	keys := make([]Nd, 0, len(graph))
+	for k := range graph {
+		keys = append(keys, k)
+	}
+}
+
+func NewDistTab(NdInit *Nd) map[*Nd]int {
+	DistTab := make(map[*Nd]int)
+	DistTab[NdInit] = 0
+
+	for _, nd := range ListeNd(graph) {
+		if nd != NdInit {
+			DistTab[nd] = Infinity
+		}
+	}
+
+	return DistTab
+}
 
 func main() {
 	getGraph(makeGraph())
-
-
-
-
 
 }
