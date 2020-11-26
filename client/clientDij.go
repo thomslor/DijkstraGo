c1d340e48ac100ca6ba89d3d2b10e515149c62d6
Estelle <esmonier01@gmail.com>
2020-11-26 17:58:06 +0100

    commentaire et reglage bugs

--- client/clientDij.go
+++ client/clientDij.go
@@ -3,15 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"log"
 	"net"
 	"os"
 	"strconv"
 	"strings"
 	"time"
 )
-/*
+
 type Nd struct {
 	nom string
 }
@@ -23,13 +21,11 @@ type Lien struct {
 }
 
 type GraphSommet struct {
-	Job bool
+	Job     bool
 	idGraph int
-	Sommet Nd
+	Sommet  Nd
 }
 
-var graph map[Nd][]Lien = makeGraph()
-*/
 func getArgs() int {
 	//Make sure we have an argument
 	if len(os.Args) != 2 {
@@ -50,54 +46,7 @@ func getArgs() int {
 	return -1
 }
 
-//fonction qui permet de créer un graphe avec un fichier .txt
-func makeGraph() map[Nd][]Lien {
-	graph := make(map[Nd][]Lien)
-	f, err := os.Open("graph.txt")
-	defer f.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rd := bufio.NewReader(f)
-
-	for {
-
-		line, err := rd.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		tsep := strings.Split(line, ";")
-		res1 := Nd{nom: tsep[0]}
-		res2 := Nd{nom: tsep[1]}
-		resq := strings.TrimSuffix(tsep[2], "\r\n")
-		res3, err := strconv.Atoi(resq)
-		if err == nil {
-			//fmt.Println(res3)
-		}
-
-		lien := Lien{res1, res2, res3}
-
-		graph[res1] = append(graph[res1], lien)
-
-		//fmt.Println("Depart:", res1, "|", "Fin:", res2, "|", "ID: ", res3, "|", "Distance:")
-	}
-	return graph
-
-}
-
-//fonction qui nous donne la liste des noeuds du graphe en entrée
-func ListeNd(graph map[Nd][]Lien) []Nd {
-	keys := make([]Nd, 0, len(graph))
-	for k := range graph {
-		keys = append(keys, k)
-	}
-	return keys
-}
+//faire une fonction qui lit le graph et le met dans un tableau (et attribue un poid random au graph)
 
 func main() {
 	//Get the port number
@@ -118,16 +67,17 @@ func main() {
 		reader := bufio.NewReader(conn)
 		fmt.Printf("#DEBUG MAIN connected\n")
 		//Client lit le graphe texte et créé un graphe
-		makeGraph()
 
 		//Client envoie le graphe lu, ligne par ligne jusqua EOF au serveur
-		for i := 0; i < len(ListeNd(graph)); i++ { //checker si ListeNd nous donne bien le nb de lignes du graphe
-			if i < len((ListeNd(graph)))-1 {
-				io.WriteString(conn, fmt.Sprintf(graph[i]))
+
+		/*for i := 0; i < len(tabGraph(graph)); i++ {
+			if i < len((tabGraph(graph)))-1 {
+				io.WriteString(conn, fmt.Sprintf(tabGraph(graph)[i]))
 			} else {
 				io.WriteString(conn, fmt.Sprintf("EOF"))
 			}
-		}
+		}*/
+
 		//Apres l'envoi, le client attend une reponse du serveur avec les chemins les plus courts
 		resultString, err := reader.ReadString('\n')
 		if err != nil {
@@ -137,11 +87,8 @@ func main() {
 		resultString = strings.TrimSuffix(resultString, "\n")
 		fmt.Printf("#DEBUG server replied : |%s|\n", resultString)
 		time.Sleep(1000 * time.Millisecond)
-			
-		//Stockage des infos recues dans un fichier texte
 
-		
-		}
+		//Stockage des infos recues dans un fichier texte
 
 	}
 
